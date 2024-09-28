1ce89a14543f9ddb833edd281eea39cbae3283db
Whsjrczr <guocindy@live.com>
2024-09-28 14:10:10 +0800

    *

--- model_exp/extension/my_module/__init__.py
+++ model_exp/extension/my_module/__init__.py
@@ -1,6 +1,6 @@
 from .view import View
 from .CCLinear import *
 from .CCConv import *
-from .normalization_scalingonly import SOLayerNorm, SOGroupNorm
+from .normalization_scalingonly import *
 from .normalization import *
 from .culculate_tools import *
