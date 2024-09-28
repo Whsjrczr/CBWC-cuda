a77bb1cd4c215504cb1b56f4a77d413689dc34d3
Whsjrczr <guocindy@live.com>
2024-09-28 18:18:56 +0800

    weight_size -> normalizated_shape

--- model_exp/extension/my_module/normalization_scalingonly.py
+++ model_exp/extension/my_module/normalization_scalingonly.py
@@ -92,27 +92,27 @@ class myLayerNorm(nn.Module):
 
 
 class SOLayerNorm(nn.Module):
-    __constants__ = ['weigth_size', 'eps', 'elementwise_affine']
-    weight_size: Tuple[int, ...]
+    __constants__ = ['normalized_shape', 'eps', 'elementwise_affine']
+    normalized_shape: Tuple[int, ...]
     eps: float
     elementwise_affine: bool
 
-    def __init__(self, weight_size, eps=1e-5, elementwise_affine=True, device=None, dtype=None):
+    def __init__(self, normalized_shape, eps=1e-5, elementwise_affine=True, device=None, dtype=None):
         super(SOLayerNorm, self).__init__()
         factory_kwargs = {'device': device, 'dtype': dtype}
-        if isinstance(weight_size, numbers.Integral):
+        if isinstance(normalized_shape, numbers.Integral):
             # mypy error: incompatible types in assignment
-            weight_size = (weight_size,)  # type: ignore[assignment]
-        self.weight_size = tuple(weight_size)  # type: ignore[arg-type]
+            normalized_shape = (normalized_shape,)  # type: ignore[assignment]
+        self.normalized_shape = tuple(normalized_shape)  # type: ignore[arg-type]
         self.eps = eps
         self.elementwise_affine = elementwise_affine
 
         if self.elementwise_affine:
-            self.weight = nn.Parameter(torch.empty(self.weight_size, **factory_kwargs))
-            self.bias = nn.Parameter(torch.empty(self.weight_size, **factory_kwargs))
+            self.weight = nn.Parameter(torch.empty(self.normalized_shape, **factory_kwargs))
+            self.bias = nn.Parameter(torch.empty(self.normalized_shape, **factory_kwargs))
         else:
-            self.weight = torch.ones(self.weight_size, **factory_kwargs)
-            self.bias = torch.zeros(self.weight_size, **factory_kwargs)            
+            self.weight = torch.ones(self.normalized_shape, **factory_kwargs)
+            self.bias = torch.zeros(self.normalized_shape, **factory_kwargs)            
 
         self.reset_parameters()
 
