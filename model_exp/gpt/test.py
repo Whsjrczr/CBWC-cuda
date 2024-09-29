4a4f5c15a2db712989d507c2060e89706e8e3394
Whsjrczr <guocindy@live.com>
2024-09-29 20:59:04 +0800

    update

--- model_exp/gpt/test.py
+++ model_exp/gpt/test.py
@@ -4,6 +4,7 @@ cache_dir = "E:\cudacode\CBWC-cuda\model_exp\gpt\model"
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2', cache_dir=cache_dir)
 model = GPT2Model.from_pretrained('gpt2', cache_dir=cache_dir)
 text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-# print(output)
+# encoded_input = tokenizer(text, return_tensors='pt')
+# output = model(**encoded_input)
+print(tokenizer)
+print(model)
--- model_exp/train.py
+++ model_exp/train.py
@@ -1,17 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import random
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 import extension as ext
 
+seed = 42
+
+# 设置随机种子
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+
+# 如果你在使用CUDA，那么还需要设置以下两个属性
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)  # 如果你使用的是多GPU
+
 # 定义超参数
 batch_size = 64
 learning_rate = 0.01
-num_epochs = 5
+# num_epochs = 5
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 # MNIST 数据集
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),transforms.Grayscale()])
@@ -36,10 +50,38 @@ class SimpleNN(nn.Module):
 
     def forward(self, input):
         return self.net(input)
+
+class SNN(nn.Module):
+    def __init__(self, depth=4, width=128, **kwargs):
+        super(SNN, self).__init__()
+        layers = [ext.View(32 * 32), ext.CCLinear(32 * 32, width), nn.LayerNorm(width), nn.ReLU(True)]
+        for _ in range(depth - 1):
+            layers.append(ext.CCLinear(width, width))
+            layers.append(nn.LayerNorm(width))
+            layers.append(nn.ReLU(True))
+        layers.append(nn.Linear(width, 10))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.net(input)
+
+class ONN(nn.Module):
+    def __init__(self, depth=4, width=128, **kwargs):
+        super(ONN, self).__init__()
+        layers = [ext.View(32 * 32), nn.Linear(32 * 32, width), ext.SOLayerNorm(width), nn.ReLU(True)]
+        for _ in range(depth - 1):
+            layers.append(nn.Linear(width, width))
+            layers.append(ext.SOLayerNorm(width))
+            layers.append(nn.ReLU(True))
+        layers.append(nn.Linear(width, 10))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.net(input)
     
 class OriginNN(nn.Module):
     def __init__(self, depth=4, width=128, **kwargs):
-        super(SimpleNN, self).__init__()
+        super(OriginNN, self).__init__()
         layers = [ext.View(32 * 32), nn.Linear(32 * 32, width), nn.LayerNorm(width), nn.ReLU(True)]
         for _ in range(depth - 1):
             layers.append(nn.Linear(width, width))
@@ -51,34 +93,49 @@ class OriginNN(nn.Module):
     def forward(self, input):
         return self.net(input)
     
-
-# 实例化网络
-model = SimpleNN().to(device)
-
-# 定义损失函数和优化器
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-# 训练网络
-for epoch in range(num_epochs):
-    model.train()
-    print(f'Begin Epoch{epoch}...')
-    for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)
-        # print("data init done...")
-        outputs = model(images)
-        # print("fp done...")
-        loss = criterion(outputs, labels)
-        # print("loss done...")
-
-        optimizer.zero_grad()
-        # print("opt init done...")
-        loss.backward()
-        # print("bp done...")
-        optimizer.step()
-        # print("opt done...")
-
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+def train(model,num_epochs):
+    
+    # 实例化网络
+    model = model.to(device)
+    
+    # 定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    
+    # 训练网络
+    for epoch in range(num_epochs):
+        model.train()
+        print(f'Begin Epoch{epoch}...')
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            # print("data init done...")
+            outputs = model(images)
+            # print("fp done...")
+            loss = criterion(outputs, labels)
+            # print("loss done...")
+    
+            optimizer.zero_grad()
+            # print("opt init done...")
+            loss.backward()
+            # print("bp done...")
+            optimizer.step()
+            # print("opt done...")
+    
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device) 
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+    
+            print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
+    
+    # 测试网络
     model.eval()
     with torch.no_grad():
         correct = 0
@@ -89,19 +146,17 @@ for epoch in range(num_epochs):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
+    
         print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
-
-# 测试网络
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images, labels = images.to(device), labels.to(device) 
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
+print("==========")
+print("CC+RMS:")
+train(SimpleNN(),20)
+print("==========")
+print("CC+LN:")
+train(SNN(),20)
+print("==========")
+print("Linear+LN:")
+train(OriginNN(),20)
+print("==========")
+print("CC+LN:")
+train(ONN(),20)
