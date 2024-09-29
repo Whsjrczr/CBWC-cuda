25733863894e146692f79b98c6d2514e96fce67e
Whsjrczr <guocindy@live.com>
2024-09-29 17:10:18 +0800

    update train

--- model_exp/train.py
+++ model_exp/train.py
@@ -14,13 +14,10 @@ num_epochs = 5
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # MNIST 数据集
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),transforms.Grayscale()])
 
-train_dataset = datasets.MNIST(root='./dataset', train=True, download=True, transform=transform)
-test_dataset = datasets.MNIST(root='./dataset', train=False, download=True, transform=transform)
+train_dataset = datasets.CIFAR10(root='./dataset/cifar10', train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR10(root='./dataset/cifar10', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
@@ -29,9 +26,9 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=Fa
 class SimpleNN(nn.Module):
     def __init__(self, depth=4, width=128, **kwargs):
         super(SimpleNN, self).__init__()
-        layers = [ext.View(28 * 28), nn.Linear(28 * 28, width), ext.SOLayerNorm(width), nn.ReLU(True)]
+        layers = [ext.View(32 * 32), ext.CCLinear(32 * 32, width), ext.SOLayerNorm(width), nn.ReLU(True)]
         for _ in range(depth - 1):
-            layers.append(nn.Linear(width, width))
+            layers.append(ext.CCLinear(width, width))
             layers.append(ext.SOLayerNorm(width))
             layers.append(nn.ReLU(True))
         layers.append(nn.Linear(width, 10))
@@ -39,6 +36,21 @@ class SimpleNN(nn.Module):
 
     def forward(self, input):
         return self.net(input)
+    
+class OriginNN(nn.Module):
+    def __init__(self, depth=4, width=128, **kwargs):
+        super(SimpleNN, self).__init__()
+        layers = [ext.View(32 * 32), nn.Linear(32 * 32, width), nn.LayerNorm(width), nn.ReLU(True)]
+        for _ in range(depth - 1):
+            layers.append(nn.Linear(width, width))
+            layers.append(nn.LayerNorm(width))
+            layers.append(nn.ReLU(True))
+        layers.append(nn.Linear(width, 10))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, input):
+        return self.net(input)
+    
 
 # 实例化网络
 model = SimpleNN().to(device)
@@ -52,18 +64,18 @@ for epoch in range(num_epochs):
     print(f'Begin Epoch{epoch}...')
     for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
-        print("data init done...")
+        # print("data init done...")
         outputs = model(images)
-        print("fp done...")
+        # print("fp done...")
         loss = criterion(outputs, labels)
-        print("loss done...")
+        # print("loss done...")
 
         optimizer.zero_grad()
-        print("opt init done...")
+        # print("opt init done...")
         loss.backward()
-        print("bp done...")
+        # print("bp done...")
         optimizer.step()
-        print("opt done...")
+        # print("opt done...")
 
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
