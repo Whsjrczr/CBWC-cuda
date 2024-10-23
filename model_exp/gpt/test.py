d8c2c1c008fbeb0e21020d8797029e4d79e49e6a
Whsjrczr <guocindy@live.com>
2024-10-23 18:49:22 +0800

    add gpt

--- model_exp/gpt/test.py
+++ model_exp/gpt/test.py
@@ -1,8 +1,8 @@
 from transformers import GPT2Tokenizer, GPT2Model
 
-cache_dir = "E:\cudacode\CBWC-cuda\model_exp\gpt\model"
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2', cache_dir=cache_dir)
-model = GPT2Model.from_pretrained('gpt2', cache_dir=cache_dir)
+cache_dir = "model_exp\gpt\model"
+tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path='gpt2', cache_dir=cache_dir, local_files_only=True)
+model = GPT2Model.from_pretrained(pretrained_model_name_or_path='gpt2', cache_dir=cache_dir, local_files_only=True)
 text = "Replace me by any text you'd like."
 # encoded_input = tokenizer(text, return_tensors='pt')
 # output = model(**encoded_input)
